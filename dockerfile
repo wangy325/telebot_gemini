b85181c41e0fb43ce0c8e6eded5e56ce5754d950
wangy325 <wangy325@qq.com>
2025-04-25 11:57:52 +0800

    bot: remove pydevd, rename dockerfile to Dockerfile

--- dockerfile
+++ dockerfile
@@ -7,6 +7,6 @@ RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 EXPOSE 8443
 WORKDIR /app
 COPY ./*.py requirements.txt dockerfile  /app/
-RUN pip install --no-cache-dir -r requirements.txt -i https://pypi.mirrors.ustc.edu.cn/simple/
+RUN pip install --no-cache-dir -r requirements.txt
 
 CMD ["python",  "main.py"]
--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import asyncio
-import bconf
 import bot
 # import to register handlers
 import handlers
 
-import pydevd_pycharm
 
-pydevd_pycharm.settrace('localhost',
-                        port=33456,
-                        suspend=False,
-                        stdoutToServer=True,
-                        stderrToServer=True)
 
 if __name__ == '__main__':
-    bconf.set_local_proxies()
     asyncio.run(bot.init_bot())
