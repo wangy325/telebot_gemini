a40025d66aa54cf9c4bac0bab7f77b5e71571f7d
wangy325 <wangy325@qq.com>
2025-04-27 16:35:16 +0800

    bot: new `tbot` branch

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ import bconf
 # import to register handlers
 import handlers
 
-import pydevd_pycharm
-
-pydevd_pycharm.settrace('localhost',
-                        port=33456,
-                        stdoutToServer=True,
-                        stderrToServer=True,
-                        suspend=False)
-
-
 
 if __name__ == '__main__':
-    bconf.set_local_proxies()
     asyncio.run(bot.init_bot())
